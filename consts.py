0950755a894ad5b3cf78ba9e4747d13f6dc983c1
Alex Barker <aemb011@gmail.com>
2023-11-27 17:02:37 -0500

    still refactoring economy cog

--- consts.py
+++ consts.py
@@ -7,11 +7,19 @@ LOG_DT_FORMAT = r'%Y-%m-%d %H:%M:%S'
 EMBED_COLOR = 0xe620c1
 
 DATA_CONSTS = {
-	"cherries"
+	"activity": {
+		"path": os.path.abspath('data/activity.json'),
+		"init_profile": {}
+	},
+	"economy": {
+		"path": os.path.abspath('data/cherries.json'),
+		"init_profile": {
+			"balance": 0,
+			"daily": (None, 0)
+		}
+	}
 }
 
-ACTIVITY_DATA = os.path.abspath('data/activity.json')
-CHERRIES_DATA = os.path.abspath('data/cherries.json')
 CHERRIES_MULTIPLIER = 10
 BALANCE = "balance"
 DAILY = "daily"
@@ -32,22 +40,24 @@ def set_data(file, data):
 	with open(file, "w") as f:
 		json.dump(data, f, indent=4, sort_keys=True)
 
-def get_user_data(user, key):
-	with open(file, )
-	data = get_data(consts.CHERRIES_DATA)
+"""
+Get user data by referencing 
+"""
+def get_user_data(data_consts_key, user, key):
+	data = get_data(DATA_CONSTS.get(data_consts_key).get("path"))
 	user_data = data.get(str(user))
 	if user_data is None:
-		user_data = init_profile()
+		user_data = DATA_CONSTS.get(data_consts_key).get("init_profile")
 		data.update(user_data)
-		consts.set_data(consts.CHERRIES_DATA, data)
+		set_data(DATA_CONSTS.get(data_consts_key).get("path"), data)
 	return user_data.get(key)
 
-def set_user_data(user, *args):
-	data = consts.get_data(consts.CHERRIES_DATA)
+def set_user_data(data_consts_key, user, *args):
+	data = get_data(DATA_CONSTS.get(data_consts_key).get("path"))
 	user_data = data.get(str(user))
 	if user_data is None:
-		user_data = init_profile()
+		user_data = DATA_CONSTS.get(data_consts_key).get("init_profile")
 	for (k,v) in args:
 		user_data.update({k: v})
 	data.update(user_data)
-	consts.set_data(consts.CHERRIES_DATA, data)
+	set_data(data_consts_key, data)
